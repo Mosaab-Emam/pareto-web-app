a2165c884c37a8daaff163849786ea531dacef3b
Mosaab Emam <mosaab.emam123@gmail.com>
2023-09-24 11:06:05 +0000

    Start with the hero section

---
+++ components/ui/progress.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+interface ProgressProps
+  extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  text?: string;
+  indicatorClassName?: string;
+}
+
+const Progress = React.forwardRef<
+  React.ElementRef<typeof ProgressPrimitive.Root>,
+  // React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+  ProgressProps
+>(({ className, value, ...props }, ref) => (
+  <ProgressPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
+      className,
+    )}
+    {...props}
+  >
+    <ProgressPrimitive.Indicator
+      className={cn(
+        "h-full w-full flex-1 bg-primary transition-all",
+        props.text ? "flex justify-center items-center" : "",
+        props.indicatorClassName,
+      )}
+      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+    >
+      {!!props.text && (
+        <div className="relative text-base translate-x-1/2 leadig-none">
+          {props.text}
+        </div>
+      )}
+    </ProgressPrimitive.Indicator>
+  </ProgressPrimitive.Root>
+));
+Progress.displayName = ProgressPrimitive.Root.displayName;
+
+export { Progress };
