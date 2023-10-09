484327f3e6e5ac08321177c1340e0dd51a3bd758
Mosaab Emam <mosaab.emam123@gmail.com>
2023-10-09 00:55:18 +0000

    ditch old hero section

--- app/page.tsx
+++ app/page.tsx
@@ -8,16 +8,6 @@ export default function Home() {
     <>
       <Hero />
       <main className="flex min-h-screen flex-col items-center justify-between p-12 md:p-24">
-        <div className="mb-12 text-7xl">
-          Initialize Your Project
-          <Progress
-            value={80}
-            text="80% done"
-            className="mt-4 block h-8"
-            indicatorClassName="bg-green-500 text-white"
-          />
-        </div>
-
         <Steps />
         <SupportBox />
       </main>
